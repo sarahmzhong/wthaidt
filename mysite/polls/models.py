ce9ed394bb9352a23c992040bda31ec5fa8b12a8
Sarah Zhong <sarahmayzhong@gmail.com>
2017-09-15 16:14:04 -0700

    tutorial part 2, skipped admin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 
@@ -9,6 +13,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -16,5 +24,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
